8dd88e7d65619dd606c818146d482c816da844be
SeungJoo-Han <andy030407@gmail.com>
2024-03-27 20:36:27 +0900

    feat: TestClient 활용한 test case 구현

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel, ValidationError, validator
+from fastapi.exceptions import HTTPException
+from fastapi.responses import PlainTextResponse
+from fastapi.testclient import TestClient
+from pydantic import BaseModel, validator
 from datetime import datetime
 
 app = FastAPI()
@@ -23,6 +26,7 @@ DB = {}
 @app.get("/")
 def read_root():
     return {"Hello, Library!"}
+
 
 @app.post("/books", response_model=Book)
 def add_book(book: Book):
@@ -53,9 +57,86 @@ def update_book(book_id: int, book: Book):
     return DB[book_id]
 
 
-@app.delete("/books/{item_id}")
+@app.delete("/books/{book_id}")
 def delete_book(book_id: int):
     if book_id not in DB:
         raise HTTPException(status_code=404, detail="Book not found")
     del DB[book_id]
     return f"Book {book_id} has been deleted."
+
+'''
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request, exc):
+    return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
+'''
+
+client = TestClient(app)
+
+
+def test_create_book():
+    new_book = {
+        "id": 0,
+        "title": "FastAPI 기초",
+        "author": "한승주",
+        "description": "test",
+        "published_year": 2023
+    }
+    response = client.post("/books", json=new_book)
+    assert response.status_code == 200
+
+def test_create_book_fail():
+    duplicated_book = {
+        "id": 0,
+        "title": "FastAPI 기초",
+        "author": "한승주",
+        "description": "test",
+        "published_year": 2023
+    }
+    response = client.post("/books", json=duplicated_book)
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Book already exists"}
+
+def test_read_book_list():
+    response = client.get("/books")
+    assert response.status_code == 200
+
+def test_read_book():
+    response = client.get("/books/0")
+    assert response.status_code == 200
+
+def test_read_book_fail():
+    response = client.get("/books/10")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Book not found"}
+
+def test_update_book():
+    update_book = {
+        "id": 0,
+        "title": "C언어 기초",
+        "author": "한승주",
+        "description": "test",
+        "published_year": 2012
+    }
+    response = client.put("/books/0", json=update_book)
+    assert response.status_code == 200
+
+def test_update_book_fail():
+    nonexistent_book = {
+        "id": 100,
+        "title": "C언어 기초",
+        "author": "한승주",
+        "description": "test",
+        "published_year": 2012
+    }
+    response = client.put("/books/100", json=nonexistent_book)
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Book not found"}
+
+def test_delete_book():
+    response = client.delete("/books/0")
+    assert response.status_code == 200
+
+def test_delete_book_fail():
+    response = client.delete("/books/100")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Book not found"}
